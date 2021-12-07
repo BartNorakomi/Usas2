0339df46f912beeb13f9198ad7fa45928b0c81db
bartfuture <bartfuture@gmail.com>
2021-12-07 17:53:05 +0200

    1

---
+++ Engine/changes.asm
@@ -0,0 +1,4 @@
+* sit punch animation is faster. Looks better now
+* sit punch now shows player with angry face
+* kick punch while jumping kicks up while jumping up and down while jumping down
+* kick jump sprites adjusted
