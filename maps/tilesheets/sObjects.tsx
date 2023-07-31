842f2fea0fbf9892236767f6e9c0bd30077ae18c
bartfuture <bartfuture@gmail.com>
2023-07-31 13:35:42 +0300

    1

--- maps/tilesheets/sObjects.tsx
+++ maps/tilesheets/sObjects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="sObjects" tilewidth="8" tileheight="8" tilecount="910" columns="35">
- <image source="../../grapx/tilesheets/sObjects.bmp" trans="ff94ff" width="280" height="208"/>
+<tileset version="1.5" tiledversion="1.7.1" name="sObjects" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="../../grapx/tilesheets/sObjects.bmp" trans="ff94ff" width="256" height="256"/>
 </tileset>
