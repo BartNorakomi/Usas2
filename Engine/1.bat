366c5cfcdaba8856dd97d8bc749c522602f29483
RomanVanDerMeulen <mobiel@romanvandermeulen.nl>
2023-11-10 15:13:16 +0100

    LabelsForRomLocations

--- Engine/1.bat
+++ Engine/1.bat
@@ -1,3 +1,6 @@
 tniasm Usas2.asm
-del tniasm.out
-del tniasm.tmp
+rem del tniasm.out
+rem del tniasm.tmp
+"C:\Program Files\openMSX\openmsx.exe" -machine Panasonic_FS-A1GT -carta "Usas2.rom" -command "set throttle off; after realtime 1 \"set throttle on\""
+
+
