b30f4d583d9f94193faf178f2e978e8039873672
RomanVanDerMeulen <mobiel@romanvandermeulen.nl>
2024-02-06 11:08:01 +0100

    x

--- Engine/enginepage3.asm
+++ Engine/enginepage3.asm
@@ -8,8 +8,8 @@ MapDataCopiedToRam:  ds  WorldMapDataMapLenght
 ;WorldMapPositionY:  db  17 | WorldMapPositionX:  db  44 ;ballroom 1 (with pipe)
 ;WorldMapPositionY:  db  20 | WorldMapPositionX:  db  44 ;ballroom 2
 ;WorldMapPositionY:  db  19 | WorldMapPositionX:  db  43 ;huge blob room
-
-WorldMapPositionY:  db  20 | WorldMapPositionX:  db  43
+roomX: equ 1*26 + "W"-"A"
+WorldMapPositionY:  db  25 | WorldMapPositionX:  db  roomX
 
 
 PlayLogo:
