439be492236d40362d7d80b15297669450d92ca3
RomanVanDerMeulen <mobiel@romanvandermeulen.nl>
2024-03-01 12:52:10 +0100

    x

--- tools/rip-robert.ps1
+++ tools/rip-robert.ps1
@@ -1,14 +1,18 @@
 
 ##### Includes #####
-. .\tiled-functions.ps1
+. .\tiled-functions.inc.ps1
+. .\Usas2-SharedFunctions.inc.ps1
 
+##### Global properties #####
+$global:usas2=get-Usas2Globals
 
-$ruinName="Pegu" #need to rip this one
-$ruinId=4
-if (-not $maps) {.\make-worldmap.ps1  -ruin $ruinId}
+$ruinName="Lemniscate" #need to rip this one
+$ruinId=2
+
+if (-not $maps) {.\make-worldmap.ps1  -ruin $ruinId} #returns $maps as a global! (yeah, I know)
 #$maps|where{$_.filename -match "as14"}
 $mapsPath="C:\Users\$($env:username)\OneDrive\Usas2\maps"
-$File="C:\Users\rvand\OneDrive\Usas2\maps\robert.tmx"
+$File="F:\RobertLemniscate.tmx"
 if (-not $robert) {[xml]$global:robert=get-content $file}
 
 $mapWidth=$robert.map.width	#in tiles
@@ -17,7 +21,8 @@ $roomWidth=38	#in tiles
 $roomHeight=27	#in tiles
 $marginX=1	#in tiles
 $marginY=1	#in tiles
-$tileset=$robert.map.tileset|where {$_.source -match $ruinName}
+$SourceTileset=$robert.map.tileset|where {$_.source -match $ruinName} #tilesheet from the source map file
+$tileset=$usas2.tiledtileset|where{$_.identity -eq $ruinname} #tilesheet
 $layers=$robert.map.layer|where{$_.visible -ne 0 -and $_.name -ne "objects"}
 
 # iterate through maps
@@ -25,7 +30,8 @@ foreach ($map in $maps)
 {	$mapX=$map.x/8	#room start positions
 	$mapy=$map.y/8
 	$xml=new-TiledMap -width $roomwidth -height $roomheight
-	$xml=add-TiledTileset -xml $xml -source $tileset.source -firstgid 1
+	#$xml=add-TiledTileset -map $xml -source $tileset.source -firstgid 1
+	$xml=add-TiledTileset -map $xml -source $tileset.path -firstgid 1
 
 	# iterate through layers
 	foreach ($layer in $layers)
@@ -38,17 +44,17 @@ foreach ($map in $maps)
 		for ($y=$mapY;$y -lt ($mapY+$roomHeight);$y++)
 		{	for ($x=$mapX;$x -lt ($mapX+$roomWidth);$x++)
 			{	$tile=$tiles[$y][$x]
-				if ($tile -ne 0) {$tile=$tile-($tileset.firstgid)+1}
+				if ($tile -ne 0) {$tile=$tile-($SourceTileset.firstgid)+1}
 				$data[$index]=$tile #write-host "$tile," -NoNewLine	
 				$index++
 			}
 		}
-		$xml=add-TiledTileLayer -xml $xml -name $layer.name -data ($data -join(","))
+		$xml=add-TiledTileLayer -map $xml -name $layer.name -data ($data -join(","))
 	}
 	
 	#$global:xml=$xml
 	"$mapspath\$($map.filename)"
-	#set-content -Value $xml.OuterXml -Path "$mapspath\$($map.filename)"
+	set-content -Value $xml.OuterXml -Path "$mapspath\$($map.filename)"  #write the file
 }
 
 #$data to raw
