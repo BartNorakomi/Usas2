26bf34d466d74448458ff5415bce7cc30bdcb080
bartfuture <bartfuture@gmail.com>
2024-05-19 21:50:51 +0300

    konark maps

--- Engine/enginepage3.asm
+++ Engine/enginepage3.asm
@@ -15,8 +15,8 @@ phase	enginepage3addr
 ;WorldMapPositionY:  db  17 | WorldMapPositionX:  db  44 ;ballroom 1 (with pipe)
 ;WorldMapPositionY:  db  20 | WorldMapPositionX:  db  44 ;ballroom 2
 ;WorldMapPositionY:  db  19 | WorldMapPositionX:  db  43 ;huge blob room
-roomX: equ ("B"-"A")*26 + "I"-"A"
-WorldMapPositionY:  db  21 | WorldMapPositionX:  db  roomX
+roomX: equ ("B"-"A")*26 + "K"-"A"
+WorldMapPositionY:  db  19 | WorldMapPositionX:  db  roomX
 
 ClesX:      dw 60 ;$19 ;230 ;250 ;210
 ClesY:      db 130 ;144-1
