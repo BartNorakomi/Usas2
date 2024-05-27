72564fbf35d22a64cf5300486826269f7e88dcb0
RomanVanDerMeulen <mobiel@romanvandermeulen.nl>
2024-05-27 13:17:13 +0200

    x

--- Engine/enginepage3.asm
+++ Engine/enginepage3.asm
@@ -15,8 +15,8 @@ phase	enginepage3addr
 ;WorldMapPositionY:  db  17 | WorldMapPositionX:  db  44 ;ballroom 1 (with pipe)
 ;WorldMapPositionY:  db  20 | WorldMapPositionX:  db  44 ;ballroom 2
 ;WorldMapPositionY:  db  19 | WorldMapPositionX:  db  43 ;huge blob room
-roomX: equ ("B"-"A")*26 + "H"-"A"
-WorldMapPositionY:  db  12 | WorldMapPositionX:  db  roomX
+roomX: equ ("B"-"A")*26 + "O"-"A"
+WorldMapPositionY:  db  19 | WorldMapPositionX:  db  roomX
 
 ClesX:      dw 60 ;$19 ;230 ;250 ;210
 ClesY:      db 130 ;144-1
