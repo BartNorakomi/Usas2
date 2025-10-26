823fe9b3ebe27df48254b71cdbb837824aba4c67
BartNorakomi <bartfuture@gmail.com>
2025-10-26 16:28:26 +0200

    1

--- grapx/_NewLevels/MushroomPlanet/mushroom.tiles.tsx
+++ grapx/_NewLevels/MushroomPlanet/mushroom.tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="mushroom.tiles" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
- <image source="mushroom.tiles.bmp" width="512" height="512"/>
+<tileset version="1.10" tiledversion="1.11.0" name="mushroom.tiles" tilewidth="8" tileheight="8" tilecount="6400" columns="32">
+ <image source="mushroom.tiles.bmp" width="256" height="256"/>
 </tileset>
