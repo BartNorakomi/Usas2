b053ff3695e79680c4e4210382f5226b7593f70e
BartNorakomi <bartfuture@gmail.com>
2025-12-01 22:40:37 +0200

    1

---
+++ grapx/_NewLevels/MushroomPlanetPortal/mushroomportal.tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="mushroomportal.tiles" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="mushroomportal.tiles.bmp" width="512" height="512"/>
+</tileset>
