6c0089b6f373b25d2b2b4f2ac19e0460970e6c3e
BartNorakomi <bartfuture@gmail.com>
2025-12-09 16:32:51 +0200

    1

--- grapx/_NewLevels/QuarryV2/quarryV2.tiles.tsx
+++ grapx/_NewLevels/QuarryV2/quarryV2.tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="quarryV2.tiles" tilewidth="8" tileheight="8" tilecount="10000" columns="100">
- <image source="quarryV2.tiles.bmp" width="800" height="800"/>
+<tileset version="1.10" tiledversion="1.11.0" name="quarryV2.tiles" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="quarryV2.tiles.bmp" width="256" height="256"/>
 </tileset>
